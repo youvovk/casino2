a93c3fa1542075596ed5e78bc34df9dd218ad622
youvovk <yuriy3vovk@gmail.com>
2019-11-19 16:25:53 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import './main.scss';
+
+export class App extends React.Component {
+  state = {
+    headerBack: false,
+  }
+
+  listenScrollEvent = e => {
+    if (window.scrollY > 50) {
+      this.setState({
+        headerBack: true,
+      })
+    } else {
+      this.setState({
+        headerBack: false,
+      })
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.listenScrollEvent);
+  }
+
+  render () {
+    const { headerBack } = this.state;
+
+    return (
+      <>
+        <div className="background">
+
+          <div className="container">
+
+            <header className={`header ${headerBack && 'header__background'}`}>
+              <div className="header__logo-wrapper">
+                <p className="header__logo">logo</p>
+                <p className="header__logo_bottom">lorem ipsum</p>
+              </div>
+
+              <input type="button" value="Register Now" className="register" />
+            </header>
+
+            <div className="main-wrapper">
+              <main className="main">
+                <p className="text text_30px">Discover a new world of casino</p>
+                  <p className="text text__extra-light text_60px wrapper">
+                    <span className="text-orange">FIRST DEPOSIT BONUS: 200% UP TO £50 +  200 BONUS</span>
+                  </p>
+                <p className="text text__extra-bold text_30px p_indent"><span className="text-red">+40 free SPINS</span></p>
+
+                <a href="#" className="register register_big">Register Now</a>
+              </main>
+            </div>
+
+          
+          </div>
+        </div>
+
+        <div className="footer-background">
+          <div className="footer-wrapper">
+            <footer className="footer">
+              <div className="footer__top">
+                  <img src="./images/visa.png" alt="" />
+                  <img src="./images/master-card.png" alt="" />
+                  <img src="./images/skrill.png" alt="" />
+                  <img src="./images/pay.png" alt="" />
+                  <img src="./images/visa.png" alt="" />
+                  <img src="./images/master-card.png" alt="" />
+                  <img src="./images/skrill.png" alt="" />
+                  <img src="./images/pay.png" alt="" />
+                  <img src="./images/visa.png" alt="" />
+                  <img src="./images/master-card.png" alt="" />
+                  <img src="./images/skrill.png" alt="" />
+              </div>
+
+              <div className="footer__bottom">
+                  <img src="./images/skrill.png" alt="" />
+                  <img src="./images/pay.png" alt="" />
+                  <img src="./images/visa.png" alt="" />
+                  <img src="./images/master-card.png" alt="" />
+                  <img src="./images/skrill.png" alt="" />
+                  <img src="./images/pay.png" alt="" />
+                  <img src="./images/visa.png" alt="" />
+              </div>
+            </footer>
+          </div>
+        </div>
+      </>
+   );
+  }
+}
+
+export default App;
