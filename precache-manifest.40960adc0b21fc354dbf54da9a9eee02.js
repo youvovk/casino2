9d2ace79efd08bf2c0fcb98cf86be7d55ddf6360
youvovk <yuriy3vovk@gmail.com>
2019-11-19 16:29:06 +0200

    Updates

---
+++ precache-manifest.40960adc0b21fc354dbf54da9a9eee02.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331ced75fe0217de90c4547e63fddf78",
+    "url": "/casino2/index.html"
+  },
+  {
+    "revision": "ff96b86593d12c075620",
+    "url": "/casino2/static/css/main.1db552ff.chunk.css"
+  },
+  {
+    "revision": "af2f98cafcfc5d7cb2da",
+    "url": "/casino2/static/js/2.6b193425.chunk.js"
+  },
+  {
+    "revision": "ff96b86593d12c075620",
+    "url": "/casino2/static/js/main.91b89386.chunk.js"
+  },
+  {
+    "revision": "4361ff7f054c83c6fbec",
+    "url": "/casino2/static/js/runtime-main.b2e71994.js"
+  },
+  {
+    "revision": "085f3ee361c9b80e0ccf8a703da9efe6",
+    "url": "/casino2/static/media/Gilroy-Light.085f3ee3.woff2"
+  },
+  {
+    "revision": "1111f8b003573d984ba8a7179862fb32",
+    "url": "/casino2/static/media/Gilroy-Light.1111f8b0.ttf"
+  },
+  {
+    "revision": "679cd8013e1e98981a3daa13995f5949",
+    "url": "/casino2/static/media/Gilroy-Light.679cd801.woff"
+  },
+  {
+    "revision": "0c03f71a39b1cb2c7fc930043adb158c",
+    "url": "/casino2/static/media/back-main.0c03f71a.jpg"
+  }
+]);
